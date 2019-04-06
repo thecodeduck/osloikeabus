4e7c0f8cfca938c834f079d5df3852870333ca12
thecodeduck <thecodeduck@gmail.com>
2019-04-06 20:44:09 +0200

    Adding disclaimer to page, changing img src for GitHub Pages hosting

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -74,15 +74,18 @@ class App extends React.Component {
 							<a href="https://www.ikea.com/no/no/stores/slependen/" className="button button-primary" target="_blank" rel="noopener noreferrer">Slependen</a>
 							<a href="https://www.ikea.com/no/no/stores/furuset/" className="button button-primary" target="_blank" rel="noopener noreferrer">Furuset</a>
 						</div>
-
+						<p>
+							⚠️ This is a personal project by <a href="https://github.com/thecodeduck/osloikeabus">Codeduck on GitHub</a> and is not affiliated or supported by IKEA Systems. Codeduck is not responsible if you miss your bus!
+						</p>
 					</div>
 					<div className="one-half column">
 						<div>
 							<p>{localizedTextTable.intro}</p>
 							<a href="https://goo.gl/maps/cy6kd5GWtGD2" target="_blank" rel="noopener noreferrer">
-								<img className="u-max-full-width" src="img/map-01.png" alt="map" />
+								<img className="u-max-full-width" src="map-01.png" alt="map" />
 							</a>
 							<a href="https://goo.gl/maps/cy6kd5GWtGD2" target="_blank" rel="noopener noreferrer">Google Maps</a>
+							<br />
 						</div>
 					</div>
 					<dialog style={modalStyle} id="modal">
